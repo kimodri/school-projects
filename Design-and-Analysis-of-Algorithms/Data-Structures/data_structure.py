c3d9c2388d25edbd174b14e913bef7902041dbb5
unknown <studies.kimaudrey@gmail.com>
2025-03-02 21:36:18 +0800

    Modify insert method

--- Design-and-Analysis-of-Algorithms/Data-Structures/data_structure.py
+++ Design-and-Analysis-of-Algorithms/Data-Structures/data_structure.py
@@ -3,95 +3,143 @@ import array
 class Array():
 
     def __init__(self, type):
-        self._array = array.array('type')
+        self.type = type
+        self._array = array.array(type)
         self.size = 0
+        self.capacity = 4
+
+    def resize(self):
+        self.capacity *= 2
+        new_array = array.array(self.type, [0] * self.capacity)
+        for i in range(self.size):
+            new_array[i] = self._array[i]
+        self._array = new_array
+
 
     # insert an element
-    def insert(self, element, position=None):
-        ''' Need to modify this to move the element and input validation try catch maybe sa GUI?'''
+    def insert(self, element, position = None):
+        pass 
 
-        if (position > self.size + 1 or position < 0):
-            print("Invalid position!")
+
+        # ''' Need to modify this to move the element and input validation try catch maybe sa GUI?'''
+
+        # # Modification with dynamic array this time
+
+        # if (position == None):
+        #     position = self.size
+
+        #     self._array[self.size] = element
+        #     self.size += 1 
+
+        # elif (position > self.size + 1 or position < 0):
+        #     print("Invalid position!")
+        #     return None
         
-        # Add the element
-        elif (position == None):
-            self._array[self.size] = element
-            self.size += 1 
-        
-        # Add the element
-        else:
-            self._array[position] = element
-            self.size += 1
-            # move the elements, slice the array
+        # # Add the element
+        # else:
+        #     #  Bago mo iinsert move mo muna by slicing
+        #     left = self._array[0:position]
+        #     right = self._array[position:]
 
+        #     addedArray = array.array('i', [])
+            
+        #     index = 0
+        #     for element in left:
+        #         addedArray[index] = element
+        #         index += 1
 
+        #     addedArray[position] = element
+        #     index += 1
+            
+        #     for element in right:
+        #         addedArray[index] = element
+        #         index += 1
 
-
+        #     self._array[position] = element
+        #     self.size += 1
+    
     # Traverse to know if the element exists
     def search(self, element):
         sortedArray = self.sort()
-        left = 0
-        right = self.size - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if sortedArray[mid] == element:
-                return mid  # Element found, return its index immediately
-            elif sortedArray[mid] < element:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return -1  # Element not found
+
+        if (sortedArray == None):
+            return None 
+        else:
+            left = 0
+            right = self.size - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if sortedArray[mid] == element:
+                    return mid  
+                elif sortedArray[mid] < element:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1  # Element not found
 
 
     def delete(self, element):
+
+        # Needs modification if there is a duplicate
         # Check if it exists
         index = self.search(element)
 
-        if (index != -1):
-            #  Will these be arrays or lists?
+        if (index == None):
+            # return None
+            print("Array is empty") #?
+
+        elif (index != -1):
             left = self._array[0:index]
-            right = self._array[-1:index:-1]  
+            right = self._array[index + 1:]  
 
-            # delete
-            for i in range(self.size - 1):
-                # decreased_arr[i] = 
-                # decreased_arr is not gonna be an array but rather a list I think. 
-                pass 
-
+            decreasedArr = array.array("i", [])                  
+           
+           # delete
+            index = 0
+            for element in left:
+                decreasedArr[index] = element
+                index += 1
+            for element in right:
+                decreasedArr[index] = element
+                index += 1
         else:
-            pass
-            # return a message that you cannot delete
+            print("The element does not exist!")
+            
 
     def sort(self, reversed = None):
         
         # copy the array so I won't have to change the unsorted array
-        copiedArray = self._array[:]
+        if (self.size == 0):
+            print("Array is empty!")
+            return None
 
-        # bubble sort
-        # ascending sort
-        n = self.size
-        if (reversed == None):
-            for i in range(n):
-                swapped = False
-                for j in range(0, n - i - 1):
-                    if copiedArray[j] > copiedArray[j + 1]:
-                        copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
-                        swapped = True
-                if not swapped:
-                    break
-            return copiedArray
-
-        # descending sort
         else:
-            for i in range(n):
-                swapped = False
-                for j in range(0, n - i - 1):
-                    if copiedArray[j] < copiedArray[j + 1]:
-                        copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
-                        swapped = True
+            copiedArray = self._array[:]
+            # bubble sort
+            # ascending sort
+            n = self.size
+            if (reversed == None):
+                for i in range(n):
+                    swapped = False
+                    for j in range(0, n - i - 1):
+                        if copiedArray[j] > copiedArray[j + 1]:
+                            copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
+                            swapped = True
                     if not swapped:
                         break
-            return copiedArray
+                return copiedArray
+
+            # descending sort
+            else:
+                for i in range(n):
+                    swapped = False
+                    for j in range(0, n - i - 1):
+                        if copiedArray[j] < copiedArray[j + 1]:
+                            copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
+                            swapped = True
+                        if not swapped:
+                            break
+                return copiedArray
 
 
 
---
+++ Design-and-Analysis-of-Algorithms/Data-Structures/main.py
@@ -0,0 +1,7 @@
+from data_structure import Array
+
+myArr = Array("i")
+
+print(myArr.size)
+myArr.insert(2)
+print(myArr.size)
