720c6230c1b96a2c64147867f8d76eff6f843b8c
unknown <studies.kimaudrey@gmail.com>
2025-03-03 11:49:59 +0800

    Fix insert

--- Design-and-Analysis-of-Algorithms/Data-Structures/data_structure.py
+++ Design-and-Analysis-of-Algorithms/Data-Structures/data_structure.py
@@ -15,10 +15,129 @@ class Array():
             new_array[i] = self._array[i]
         self._array = new_array
 
-
     # insert an element
     def insert(self, element, position = None):
-        pass 
+        if (position == None):
+            position = self.size
+
+        if (position < 0 or position > self.size):
+            print("Invalid position!")
+            return None 
+
+        if (self.size == self.capacity):
+            self.resize()
+        
+        new_array = array.array(self.type, [0] * self.capacity)
+
+        for i in range(position):
+            new_array[i] = self._array[i]
+
+        new_array[position] = element 
+
+        for i in range(position, self.size):
+            new_array[i + 1] = self._array[i]
+
+        
+        self.size += 1
+
+        perfect_array = array.array('i', [0] * self.size)
+
+        for i in range(self.size):
+            perfect_array[i] = new_array[i]
+        
+        self._array = perfect_array 
+
+        return self._array
+ 
+    # Traverse to know if the element exists
+    def search(self, element):
+        sortedArray = self.sort()
+        if (sortedArray == None):
+            return None 
+        else:
+            left = 0
+            right = self.size - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if sortedArray[mid] == element:
+                    return mid  
+                elif sortedArray[mid] < element:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1  # Element not found
+
+    def delete(self, element):
+
+        # Needs modification if there is a duplicate
+        # Check if it exists
+        index = self.search(element)
+
+        if (index == None):
+            # return None
+            print("Array is empty") #?
+
+        elif (index != -1):
+            left = self._array[0:index]
+            right = self._array[index + 1:]  
+
+            decreasedArr = array.array("i", [])                  
+           
+           # delete
+            index = 0
+            for element in left:
+                decreasedArr[index] = element
+                index += 1
+            for element in right:
+                decreasedArr[index] = element
+                index += 1
+        else:
+            print("The element does not exist!")
+            
+    def sort(self, reversed = None):
+        # copy the array so I won't have to change the unsorted array
+        if (self.size == 0):
+            print("Array is empty!")
+            return None
+
+        else:
+            copiedArray = self._array[:]
+            # bubble sort
+            # ascending sort
+            n = self.size
+            if (reversed == None):
+                for i in range(n):
+                    swapped = False
+                    for j in range(0, n - i - 1):
+                        if copiedArray[j] > copiedArray[j + 1]:
+                            copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
+                            swapped = True
+                    if not swapped:
+                        break
+                return copiedArray
+
+            # descending sort
+            else:
+                for i in range(n):
+                    swapped = False  # Reset swapped here, before the inner loop.
+                    for j in range(0, n - i - 1):
+                        if copiedArray[j] < copiedArray[j + 1]:
+                            copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
+                            swapped = True
+                    if not swapped:
+                        break
+                return copiedArray
+
+
+        
+
+
+
+
+
+       # self.size += 1
+        # new_arr = array.array(type, [0] * self.size) 
+        # magkakaroon ng problems sa assignment with self.size
 
 
         # ''' Need to modify this to move the element and input validation try catch maybe sa GUI?'''
@@ -57,89 +176,3 @@ class Array():
 
         #     self._array[position] = element
         #     self.size += 1
-    
-    # Traverse to know if the element exists
-    def search(self, element):
-        sortedArray = self.sort()
-
-        if (sortedArray == None):
-            return None 
-        else:
-            left = 0
-            right = self.size - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if sortedArray[mid] == element:
-                    return mid  
-                elif sortedArray[mid] < element:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return -1  # Element not found
-
-
-    def delete(self, element):
-
-        # Needs modification if there is a duplicate
-        # Check if it exists
-        index = self.search(element)
-
-        if (index == None):
-            # return None
-            print("Array is empty") #?
-
-        elif (index != -1):
-            left = self._array[0:index]
-            right = self._array[index + 1:]  
-
-            decreasedArr = array.array("i", [])                  
-           
-           # delete
-            index = 0
-            for element in left:
-                decreasedArr[index] = element
-                index += 1
-            for element in right:
-                decreasedArr[index] = element
-                index += 1
-        else:
-            print("The element does not exist!")
-            
-
-    def sort(self, reversed = None):
-        
-        # copy the array so I won't have to change the unsorted array
-        if (self.size == 0):
-            print("Array is empty!")
-            return None
-
-        else:
-            copiedArray = self._array[:]
-            # bubble sort
-            # ascending sort
-            n = self.size
-            if (reversed == None):
-                for i in range(n):
-                    swapped = False
-                    for j in range(0, n - i - 1):
-                        if copiedArray[j] > copiedArray[j + 1]:
-                            copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
-                            swapped = True
-                    if not swapped:
-                        break
-                return copiedArray
-
-            # descending sort
-            else:
-                for i in range(n):
-                    swapped = False
-                    for j in range(0, n - i - 1):
-                        if copiedArray[j] < copiedArray[j + 1]:
-                            copiedArray[j], copiedArray[j + 1] = copiedArray[j + 1], copiedArray[j]
-                            swapped = True
-                        if not swapped:
-                            break
-                return copiedArray
-
-
-
--- Design-and-Analysis-of-Algorithms/Data-Structures/main.py
+++ Design-and-Analysis-of-Algorithms/Data-Structures/main.py
@@ -2,6 +2,26 @@ from data_structure import Array
 
 myArr = Array("i")
 
-print(myArr.size)
+
 myArr.insert(2)
-print(myArr.size)
+myArr.insert(4)
+myArr.insert(9)
+myArr.insert(1)
+myArr.insert(7)
+myArr.insert(8)
+myArr.insert(3)
+myArr.insert(5)
+myArr.insert(0)
+myArr.insert(6)
+
+
+
+print([x for x in myArr._array])
+print(list(myArr.sort()))
+print(list(myArr.sort(reversed=True)))
+
+print(myArr.search(1))
+print(myArr.search(11))
+print(myArr.search(69))
+
+
